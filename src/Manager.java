eb501267995a173b521221f62e105707bfa6b508
Dajumaks <Daniel09847montoya@gmail.com>
2025-08-25 19:56:08 -0500

    add manager Class and Modify Main

---
+++ src/Manager.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Manager {
+    Private List<Integer> numeros;
+
+    public Manager() {
+        this.numeros = new ArrayList<>();
+    }
+    public void addNumero(int numero){
+        this.numeros.add(numero);
+
+    }
+}
